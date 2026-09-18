a147977ea3ab45de6dc5df52100855d066fdcf77
reykoki <reykokimonster@gmail.com>
2026-09-18 20:50:55 +0000

    all ordinal

--- deep_learning/CloudDataset.py
+++ deep_learning/CloudDataset.py
@@ -22,17 +22,12 @@ class CloudDataset(Dataset):
         truth_tensor = torch.from_numpy(truth_img).long()
         truth_tensor = truth_tensor.permute(2, 0, 1)
 
-#        cod_therm = (truth_tensor[0].unsqueeze(0) >= torch.arange(1, 6).view(5, 1, 1)).float()
+        cod_therm = (truth_tensor[0].unsqueeze(0) >= torch.arange(1, 6).view(5, 1, 1)).float()
         ctt_therm = (truth_tensor[1].unsqueeze(0) >= torch.arange(1, 6).view(5, 1, 1)).float()
-#        ctp_therm = (truth_tensor[2].unsqueeze(0) >= torch.arange(1, 6).view(5, 1, 1)).float()
+        ctp_therm = (truth_tensor[2].unsqueeze(0) >= torch.arange(1, 6).view(5, 1, 1)).float()
 #        transition = truth_tensor[3]
 #
-#        truth_tensor = torch.cat([
-#            cod_therm,
-#            ctt_therm,
-#            ctp_therm,
-#            transition.unsqueeze(0).float()
-#        ], dim=0)
+        #truth_tensor = torch.cat([ cod_therm, ctt_therm, ctp_therm, transition.unsqueeze(0).float() ], dim=0)
+        truth_tensor = torch.cat([ cod_therm, ctt_therm, ctp_therm], dim=0)
 #
-#        return data_tensor, truth_tensor
-        return data_tensor, ctt_therm 
+        return data_tensor, truth_tensor
